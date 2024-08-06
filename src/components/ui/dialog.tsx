e9244db863a0fac543c2beb955153fafd0eea87e
Gaurav Subedi <ergauravsubedi@gmail.com>
2024-08-06 12:32:03 +0545

    chore(lint-staged): add eslint-config-prettier package

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -65,11 +65,7 @@ const DialogTitle = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Title>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
 >(({ className, ...props }, ref) => (
-  <DialogPrimitive.Title
-    ref={ref}
-    className={cn('text-2xl font-bold leading-none', className)}
-    {...props}
-  />
+  <DialogPrimitive.Title ref={ref} className={cn('text-2xl font-bold leading-none', className)} {...props} />
 ));
 DialogTitle.displayName = DialogPrimitive.Title.displayName;
 
